b5e5609deadf37e8d1d5f60745eb5519b4cbaa1c
Trust <trustmcdaniel@gmail.com>
2022-11-14 14:39:29 -0500

    project complete

---
+++ python_app.py
@@ -0,0 +1,8 @@
+def replace_word():
+    
+    str = "Hello! I'm Trust, have a good day"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word to replace: ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+replace_word()
